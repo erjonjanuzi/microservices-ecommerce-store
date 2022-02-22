056713ba1becb5a713b1a04d2f09281163d4ff88
erjonjanuzi <erjonjanuzi2018@gmail.com>
2022-02-22 21:02:34 +0100

    revert change

--- users/src/routes/allUsers.ts
+++ users/src/routes/allUsers.ts
@@ -1,7 +1,7 @@
 import { requireAuth } from '@labcourseapp/common';
 import express from 'express';
 import { User } from '../models/user';
-console.log('test');
+
 const router = express.Router();
 
 router.get('/api/users/all', requireAuth, async (req, res) => {
