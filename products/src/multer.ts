cffd361ec8210cf4b7b5f46abb41002aa253a629
erjonjanuzi <erjonjanuzi2018@gmail.com>
2022-03-07 08:26:10 +0100

    upload product images initial setup

---
+++ products/src/multer.ts
@@ -0,0 +1,14 @@
+import multer from 'multer';
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './uploads');
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, new Date().toISOString() + '-' + file.originalname);
+//     },
+// });
+
+const upload = multer()
+
+export { upload };
--- products/src/routes/createProduct.ts
+++ products/src/routes/createProduct.ts
@@ -1,67 +1,81 @@
 import { BadRequestError, currentUser, NotAuthorizedError, requireAuth, Roles, validateRequest, adminRoute } from '@labcourseapp/common';
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Express } from 'express';
 import { body } from 'express-validator';
 import { ProductCreatedPublisher } from '../events/publishers/ProductCreatedPublisher';
 import { Product } from '../models/product';
 import { natsWrapper } from '../natsWrapper';
+import cloudinary from 'cloudinary'
+import { upload } from '../multer';
+import { Multer } from 'multer';
 
 const router = express.Router();
 
 router.post(
     '/api/products',
-    requireAuth,
-    adminRoute,
-    [
-        body('title').not().isEmpty().withMessage('Title is required'),
-        body('price')
-            .isFloat({ gt: 0 })
-            .withMessage('Price must be greater than zero'),
-        body('quantity').not().isEmpty().withMessage('Quantity is required'),
-        body('quantity').isInt().withMessage('Quantity must be a whole number'),
-        body('description')
-            .not()
-            .isEmpty()
-            .withMessage('Description is required'),
-        body('category').not().isEmpty().withMessage('Category is required'),
-        body('images')
-            .isArray({ min: 1 })
-            .withMessage('At least one image is required'),
-    ],
+    // requireAuth,
+    // [
+    //     body('title').not().isEmpty().withMessage('Title is required'),
+    //     body('price')
+    //         .isFloat({ gt: 0 })
+    //         .withMessage('Price must be greater than zero'),
+    //     body('quantity').not().isEmpty().withMessage('Quantity is required'),
+    //     body('quantity').isInt().withMessage('Quantity must be a whole number'),
+    //     body('description')
+    //         .not()
+    //         .isEmpty()
+    //         .withMessage('Description is required'),
+    //     body('category').not().isEmpty().withMessage('Category is required'),
+    //     body('images')
+    //         .isArray({ min: 1 })
+    //         .withMessage('At least one image is required'),
+    // ],
+    upload.array('images'),
     validateRequest,
     async (req: Request, res: Response) => {
-        const { title, price, quantity, description, category, images } =
+
+        const images = req.files
+
+        const { title, price, quantity, description, category } =
             req.body;
 
-        let count = 0;
-        images.forEach((image: {url: string, isMain?: boolean}) => {
-            if (image.isMain) count++;
-            if (count > 1) throw new BadRequestError('Only one image can be main image');
-        });
-            
-        const product = Product.build({
-            title,
-            price,
-            quantity,
-            description,
-            category,
-            images,
-        });
-        await product.save();
+        // let count = 0;
+        // images.forEach((image: {url: string, isMain?: boolean}) => {
+        //     if (image.isMain) count++;
+        //     if (count > 1) throw new BadRequestError('Only one image can be main image');
+        // });
 
-        await new ProductCreatedPublisher(natsWrapper.client).publish({
-            id: product.id,
-            version: product.version,
-            title: product.title,
-            price: product.price,
-            quantity: product.quantity,
-            description: product.description,
-            category: product.category,
-            rating: product.rating,
-            sale: product.sale,
-            images: product.images
-        })
+        const urls = [];
+        // @ts-ignore
+        for (const image of images){
+            const {path} = image
+            const newPath = await cloudinary.v2.uploader.upload(path)
+            urls.push(newPath)
+        }
 
-        res.status(201).send(product);
+        // const product = Product.build({
+        //     title,
+        //     price,
+        //     quantity,
+        //     description,
+        //     category,
+        //     images,
+        // });
+        // await product.save();
+
+        // await new ProductCreatedPublisher(natsWrapper.client).publish({
+        //     id: product.id,
+        //     version: product.version,
+        //     title: product.title,
+        //     price: product.price,
+        //     quantity: product.quantity,
+        //     description: product.description,
+        //     category: product.category,
+        //     rating: product.rating,
+        //     sale: product.sale,
+        //     images: product.images
+        // })
+
+        res.status(201).send(urls);
     }
 );
 
