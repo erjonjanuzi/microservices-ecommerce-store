b6183ffb96464bbc4779c9e1a564fef4c79aedee
erjonjanuzi <erjonjanuzi2018@gmail.com>
2022-03-09 20:42:01 +0100

    orders service setup

---
+++ orders/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { currentUser, errorHandler, NotFoundError } from '@labcourseapp/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        // secure: process.env.NODE_ENV !== 'test',
+        secure: false,
+    })
+);
+app.use(currentUser);
+
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
