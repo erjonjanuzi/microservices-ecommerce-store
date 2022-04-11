8e368b500ba8ca167b25288e9ff5080fbab068aa
erjonjanuzi <erjonjanuzi2018@gmail.com>
2022-04-11 16:34:05 +0200

    implement new mail service

---
+++ users/src/services/MailService.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+
+export class MailService {
+    private SERVICE = 'gmail';
+    private senderEmail;
+    private transporter;
+
+    constructor(senderEmail: string) {
+        this.senderEmail = senderEmail;
+        this.transporter = nodemailer.createTransport({
+            service: this.SERVICE,
+            auth: {
+                user: this.senderEmail,
+                pass: 'wtqdlvxuwimeebui'
+            }
+        })
+    }
+
+    async send(to: string, subject: string, html: string) {
+        await this.transporter.sendMail({from: this.senderEmail, to, subject, html})
+    }
+}
